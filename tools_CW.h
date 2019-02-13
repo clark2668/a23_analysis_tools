58ba60a193042631477b07fa08f1dc54653c3388
Brian Clark <clark.2668@osu.edu>
2019-02-13 12:07:06 -0500

    Add testbed CW cut / identifier from the CWID program

--- tools_CW.h
+++ tools_CW.h
@@ -321,3 +321,291 @@ TGraph *getPhaseVariance( vector<deque<TGraph*> > vdGrPhaseDiff ){
   return gSigmaVarianceAvg;
 
 }
+
+vector<double> CWCut_TB(vector <TGraph*> waveforms, vector <TGraph*> baselines, int pol, double dBCut, double dBCutBroad, int station, int num_coinc){
+  double lowFreqLimit=120.;
+  double highFreqLimit=900.;
+  double halfrange = (highFreqLimit - lowFreqLimit)/2.;
+  double halfway = double(lowFreqLimit + halfrange);
+  const int numAnts = 16;
+  double deltaTInt = 0.6;
+
+  vector < vector < double> > badFreqs;
+  badFreqs.resize(numAnts);
+  vector < vector < double> > badFreqsBroad;
+  badFreqsBroad.resize(numAnts);
+
+  TGraph *baseline_clone[numAnts];
+  vector <TGraph*> newFFTs;
+  vector <TGraph*> newBaselines;
+
+  double deltaF_save;
+
+  AraGeomTool *geomTool = AraGeomTool::Instance();
+
+  for(int ant=0; ant<numAnts; ant++){
+
+    double magFFT[2000];
+    double magFFTBegin[2000];
+    double frequencyArray[2000];
+    for(int i=0; i<2000; i++){
+      magFFT[i]=0;
+      frequencyArray[i]=-1;
+    }
+
+    int WaveformLength = 2048; //big, unfortunately...
+    //what comes next is a not-so-obvious (imo) way of padding the waveform
+    TGraph *chan1Int = FFTtools::getInterpolatedGraph(waveforms[ant],deltaTInt);
+    double *getX = chan1Int->GetX();
+    double deltaT = getX[1]-getX[0];
+    while(chan1Int->GetN() < WaveformLength){
+      double lastX = 0.;
+      double lastY = 0.;
+      chan1Int->GetPoint(chan1Int->GetN()-1,lastX,lastY);
+      chan1Int->SetPoint(chan1Int->GetN(), lastX + deltaT, 0 );
+    }
+    double *getY = chan1Int->GetY();
+    int length=chan1Int->GetN();
+    FFTWComplex *theFFT=FFTtools::doFFT(length,getY);
+
+    int newLength=(length/2)+1;
+    double deltaF=1/(deltaT*length); //Hz
+    deltaF*=1e3; //MHz
+    deltaF_save=deltaF;
+
+    for(int i=1;i<newLength;i++) {
+      if (i==0) frequencyArray[i]=0.;
+      if (i>0) frequencyArray[i]=frequencyArray[i-1]+deltaF;
+      if (frequencyArray[i]>=lowFreqLimit && frequencyArray[i]<=highFreqLimit){
+        magFFT[i]+=theFFT[i].re*theFFT[i].re+theFFT[i].im*theFFT[i].im;
+      }
+      else{
+        magFFT[i]=-1000;
+      }
+    }
+    delete chan1Int;
+    delete [] theFFT;
+
+    for(int i=0;i<newLength;i++) {
+      if (frequencyArray[i]>=lowFreqLimit && frequencyArray[i]<=highFreqLimit){
+        magFFT[i]=10*log10(sqrt(magFFT[i]));
+      }
+      else magFFT[i]=-1000;
+    }
+    
+    //need to copy the baselines into new graphs
+    double xx, yy;
+    double bXreal[newLength];
+    double bYreal[newLength];
+
+    baseline_clone[ant] = new TGraph();
+
+    for (int i3 = 0; i3 < newLength; i3++){
+      baselines[ant]->GetPoint(i3, xx, yy);
+      baseline_clone[ant]->SetPoint(i3, xx, yy);
+      bXreal[i3] = xx;
+      bYreal[i3] = yy;
+    }
+    double *bY = baselines[ant]->GetY();
+    double *bX = baselines[ant]->GetX();
+    int n = baselines[ant]->GetN();
+
+    double *bY_unmodified=baseline_clone[ant]->GetY();
+    double *bX_unmodified=baseline_clone[ant]->GetX();
+    int n_unmodified=baseline_clone[ant]->GetN();
+
+    
+    // Calculate mean baseline
+    //get baseline average so we can bump FFT around
+    double mean=0;    
+    double meanBaseline=0;
+    int navg=0;
+    int nfirsthalfavg=0;
+    int nsecondhalfavg=0;
+    double firstHalfMeanBaseline=0;
+    double secondHalfMeanBaseline=0;
+    double firstHalfMean=0;
+    double secondHalfMean=0;
+
+    for (int i=0;i<n;i++){
+      if (bX[i]>=lowFreqLimit && bX[i]<highFreqLimit){
+        meanBaseline+=bY[i];
+        navg++;
+      }
+      if (bX[i]>=lowFreqLimit && bX[i]<halfway){
+        firstHalfMeanBaseline+=bY[i];
+        nfirsthalfavg++;
+      }
+      if (bX[i]>=halfway && bX[i]<highFreqLimit){
+        secondHalfMeanBaseline+=bY[i];
+        nsecondhalfavg++;
+      }
+    }
+    meanBaseline=meanBaseline/double(navg);
+    firstHalfMeanBaseline=firstHalfMeanBaseline/double(nfirsthalfavg);
+    secondHalfMeanBaseline=secondHalfMeanBaseline/double(nsecondhalfavg);
+
+    navg=0;
+    nfirsthalfavg=0;
+    nsecondhalfavg=0;
+
+    //get average of graph in question
+    for (int i=0;i<newLength;i++){
+      if (frequencyArray[i]>=lowFreqLimit && frequencyArray[i]<highFreqLimit){
+        mean+=magFFT[i];
+        navg++;
+      }
+      if (frequencyArray[i]>=lowFreqLimit && frequencyArray[i]<halfway){
+        firstHalfMean+=magFFT[i];
+        nfirsthalfavg++;
+      }
+      if (frequencyArray[i]>=halfway && frequencyArray[i]<highFreqLimit){
+        secondHalfMean+=magFFT[i];
+        nsecondhalfavg++;
+      }
+    }
+    mean=mean/double(navg);
+    firstHalfMean=firstHalfMean/double(nfirsthalfavg);
+    secondHalfMean=secondHalfMean/double(nsecondhalfavg); 
+
+    //now bump the average to the baseline average and apply a tilt correction to baseline
+    double deltaMean=mean-meanBaseline;
+    double deltaMeanFirst=firstHalfMean-firstHalfMeanBaseline-deltaMean;
+    double deltaMeanSecond=secondHalfMean-secondHalfMeanBaseline-deltaMean;
+    double slope=(deltaMeanFirst-deltaMeanSecond)/halfrange;
+
+    for (int i=0;i<newLength;i++){
+      magFFTBegin[i]=magFFT[i];
+      magFFT[i]=magFFT[i]-deltaMean;
+    }
+    for (int ctr=0;ctr<n;ctr++){
+      if (bX[ctr]>=lowFreqLimit && bX[ctr]<highFreqLimit){
+        bYreal[ctr]=bYreal[ctr]-slope*(bXreal[ctr]-halfway);
+      }
+    }
+    
+    //now see if any peaks are ndB above the baseline.
+    double deltaMag[newLength];
+    
+    int j;
+    for (int i=0;i<newLength;i++){
+      if (frequencyArray[i]>lowFreqLimit+2 && frequencyArray[i]<highFreqLimit-2){
+        for (j=0;j<n;j++){
+          if (bX[j]>frequencyArray[i]) break; // finds bin where frequencies match
+        }
+        deltaMag[i]=magFFT[i]-bYreal[j]; // changed
+      }
+      else deltaMag[i]=-1000;
+    }
+
+    TGraph *magFFTgraph = new TGraph(2000,frequencyArray,magFFT);
+    TGraph *newBaseline = new TGraph(2000,frequencyArray,bYreal);
+    newFFTs.push_back(magFFTgraph);
+    newBaselines.push_back(newBaseline);
+
+    int index;
+    for (int bin = 0; bin < newLength; bin++){
+      if (deltaMag[bin] > dBCut){
+        badFreqs[ant].push_back(frequencyArray[bin]);
+      }
+      if (deltaMag[bin] > dBCutBroad){
+        badFreqsBroad[ant].push_back(frequencyArray[bin]);
+      }
+    }
+  } //loop over antennas
+
+  vector< vector <double> > freqMatching;
+  double freqRangeBroad=40.;
+  double freqRangeBroad_bins = freqRangeBroad/deltaF_save; //number of bins spanned by 40 MHz
+  vector<double> FreqToNotch;
+
+  for(int i=0; i<numAnts-1; i++){
+    int i_pol = geomTool->getStationInfo(station)->getAntennaInfo(i)->polType;
+    if(i_pol!=pol) continue; //check polarization
+    for(int ii=0; ii<int(badFreqs[i].size()); ii++){ //loop over the bad frequencies for this antenna
+      int matchedFreqs=1;
+      int matchedFreqsFull=0;
+      bool broad_band1=false;
+      bool broad_band2=false;
+      int broad_freqs1=0;
+      int broad_freqs2=0;
+
+      //loop over the other frequencies which were tagged as being above the "broad" threshold in this channel
+      for(int ii2=0; ii2<int(badFreqsBroad[i].size()); ii2++){
+        //if that frequency is w/in freqRangeBroad window
+        if((abs(badFreqs[i][ii] - badFreqsBroad[i][ii2]) < freqRangeBroad)){
+          broad_freqs1++;
+        }
+      }
+
+      //if there are frequencies over the broad threshold w/in 40 MHz
+      //then we want to know if the number of contaminated bins/number of bins in 40 MHz < 50%
+      //if it's larger, then it's broadband, and we shouldn't touch it!
+      if( (freqRangeBroad_bins) != 0){
+        if(double(broad_freqs1-1)/double(int(freqRangeBroad_bins)-1) > 0.5){
+          broad_band1=true;
+        }
+        else{
+          matchedFreqsFull++;
+        }
+      }
+      else{
+        matchedFreqsFull++;
+      }
+
+      //now loop over all the other antennas in the array
+      for(int j=i+1; j<numAnts; j++){
+        int j_pol = geomTool->getStationInfo(station)->getAntennaInfo(j)->polType;
+        if(j_pol!=i_pol) continue; //check polarization agreement
+        bool matched_ant = false;
+        //check all of their bad frequencies
+        for(int jj=0; (jj<int(badFreqs[j].size()) && matched_ant==false); jj++){
+          //if their bad frequencies happens within 5 MHz of the first bad frequency
+          //we know something is up
+          if((abs(badFreqs[i][ii] - badFreqs[j][jj]) < 5.)){
+
+            broad_freqs2=0;
+            matchedFreqs++;
+            matched_ant=true;
+            //now check all of the bad frequencies in this secondary antenna
+                        for (int jj2 = 0; jj2 < int(badFreqsBroad[j].size()); jj2++){
+                          //if it's trouble make frequencies are w/in 40 MHz, then we want to record that
+                          if ((abs(badFreqs[j][jj] - badFreqsBroad[j][jj2]) < freqRangeBroad)){
+                            broad_freqs2++;
+                          }
+                        }
+                        //if there are frequencies over the broad threshold w/in 40 MHz
+            //then we want to know if the number of contaminated bins/number of bins in 40 MHz < 50%
+            //if it's larger, then it's broadband, and we shouldn't touch it!
+                        if(freqRangeBroad_bins!=0){
+                          if(double(broad_freqs2-1)/double(int(freqRangeBroad_bins)-1) > 0.5){
+                            broad_band2=true;
+                          }
+                        }
+                        //if the first ant was *not* broadband, and *neither* was this one
+                        //then we should say "yeah, we found something narrow, please notch me"
+                        if((broad_band1==false) && (broad_band2==false)){
+                          matchedFreqsFull++;
+                        } //were the trouble frequencies for both ants independently not broadband
+          } //was the second ant's bad frequency within 5 MHz of the first antennas bad frequency?
+        } //loop over second antennas bad freqs
+      }//loop over second antenna
+      if(matchedFreqsFull>=num_coinc){
+        double new_freq=badFreqs[i][ii];
+        for(int k=0; k<FreqToNotch.size(); k++){
+          if(abs(new_freq)-FreqToNotch[k] < 0.01)
+            new_freq=false;
+        }
+        if(new_freq)
+          FreqToNotch.push_back(badFreqs[i][ii]);
+      }
+    }//loop over trouble frequencies for antenna 1
+  } //loop over antenna 1
+
+  for(int i=0; i<16; i++){
+    delete baseline_clone[i];
+    delete newFFTs[i];
+    delete newBaselines[i];
+  }
+  return FreqToNotch;
+}
