56f1d5301332233814cae67c80562d4fc16b3239
Brian Clark <clark.2668@osu.edu>
2019-03-20 13:52:29 -0400

    remove huge number of unnecessary includes from tools_RecoFuns

--- tools_RecoFns.h
+++ tools_RecoFns.h
@@ -5,53 +5,9 @@
 /////     Functions for making a variety of cuts on Ara Data             ///// 
 /////  Author: CGP, adapted from ACG, class structure from RJN           ///// 
 ////////////////////////////////////////////////////////////////////////////// 
-//System includes                                                                                                               
-#include <fstream>
-#include <iostream>
-#include <stdio.h>
-#include <cstdlib>
-#include <sstream>
-#include <functional>
-#include <numeric>
-#include <deque>
+//includes                                                                                                             
 using namespace std;
-
-//AraRoot Includes                                                                                                              
-#include "RawIcrrStationEvent.h"
-#include "RawAtriStationEvent.h"
-#include "UsefulAraStationEvent.h"
-#include "UsefulIcrrStationEvent.h"
-#include "UsefulAtriStationEvent.h"
-#include "AraGeomTool.h"
-#include "AraAntennaInfo.h"
-
-#include "FFTtools.h"
-
-//ROOT Includes                                                                                                                 
-#include "TROOT.h"
-#include "TRandom3.h"
-#include "TCanvas.h"
-#include "TTree.h"
-#include "TFile.h"
-#include "TButton.h"
-#include "TGroupButton.h"
-#include <TGClient.h>
-#include "TStyle.h"
-#include "TPostScript.h"
-#include "TTree.h"
-#include "math.h"
-#include "TText.h"
-#include "TF1.h"
-#include "TH2F.h"
-#include "TLegend.h"
-#include "TNtuple.h"
-#include "TProfile.h"
-#include "Math/Interpolator.h"
-#include "TImage.h"
-#include "TMarker.h"
-#include "TStyle.h"
-
-#include "tools_Constants.h"
+#include "TH2D.h"                                                                                                              
 
 void getCorrMapPeak_wStats( TH2D *theCorrMap_input, int &peakTheta, int &peakPhi, double &peakCorr, double &minCorr, double &meanCorr, double &rmsCorr, double &peakSigma) {
 	
